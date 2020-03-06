5031e7a48a0ceffa7eb799db62137f2da4d03a44
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-06 15:59:18 +0900

    m

---
+++ app/controllers/fixedcosts_controller.rb
@@ -0,0 +1,45 @@
+class FixedcostsController < ApplicationController
+  def index
+		@fixedcosts = Fixedcost.order(created_at: :asc)
+	end
+ 
+	def show
+		@fixedcost = Fixedcost.find(params[:id])
+	end
+ 
+	def new
+		@fixedcost = Fixedcost.new()
+	end
+ 
+	def edit
+		@fixedcost = Fixedcost.find(params[:id])
+	end
+ 
+	def create
+		@fixedcost = Fixedcost.new(params[:fixedcost])
+		if @fixedcost.save
+			redirect_to @fixedcost, notice: "つかったおかねをとうろくしたよ"
+		else
+			render "new"
+		end
+ 
+	end
+ 
+	def update
+		@fixedcost = Fixedcost.find(params[:id])
+		@fixedcost.assign_attributes(params[:fixedcost])
+		if @fixedcost.save
+			redirect_to @fixedcost, notice: "つかったおかねをとうろくしたよ"
+		else
+			render "new"
+		end
+ 
+ 
+	end
+ 
+	def destroy
+		@fixedcost = Fixedcost.find(params[:id])
+		@fixedcost.destroy
+		redirect_to :fixedcosts, notice: "つかったおかねをけしたよ"
+	end
+end
---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,42 @@
+class IncomesController < ApplicationController
+  def index
+    @incomes = Income.order(created_at: :asc)
+	end
+ 
+  def show
+    @income = Income.find(params[:id])
+	end
+ 
+  def new
+    @income = Income.new()
+	end
+ 
+  def edit
+    @income = Income.find(params[:id])
+	end
+ 
+  def create
+    @income = Income.new(params[:income])
+    if  @income.save
+        redirect_to @income,notice:"とうろくしたよ"
+    else
+      render "new"
+    end
+	end
+ 
+  def update
+    @income = Income.find(params[:id])
+    @income.assign_attributes(params[:income])
+    if @income.save
+       redirect_to @income, notice: "おしごとが作れたよ！"
+    else
+      render "new"
+    end
+  end
+ 
+  def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+     redirect_to :incomes, notice: "けしたよ！"
+	end
+end

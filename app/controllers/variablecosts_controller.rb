d7d2eecf3a9793124155e7685d171252ba570f73
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-06 17:35:35 +0900

    お

---
+++ app/controllers/variablecosts_controller.rb
@@ -0,0 +1,44 @@
+class VariablecostsController < ApplicationController
+ 
+  def index
+    @variablecosts = Variablecost.order(created_at: :asc)
+  end
+
+  def show
+    @variablecost = Variablecost.find(params[:id])
+  end
+
+  def new
+    @variablecost = Variablecost.new()
+  end
+
+  def edit
+    @variablecost = Variablecost.find(params[:id])
+  end
+
+  def create
+    @variablecost = Variablecost.new(params[:variablecost])
+    if @variablecost.save
+      redirect_to @variablecost, notice: "ボーナスをとうろくしたよ"
+    else
+      render "new"
+    end
+
+  end
+
+  def update
+    @variablecost = Variablecost.find(params[:id])
+    @variablecost.assign_attributes(params[:variablecost])
+    if @variablecost.save
+      redirect_to @variablecost, notice: "ボーナスをとうろくしたよ"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @variablecost = Variablecost.find(params[:id])
+    @variablecost.destroy
+    redirect_to :variablecosts, notice: "けしたよ！"
+  end
+
